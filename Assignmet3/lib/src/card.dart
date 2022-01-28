8cb30ee377ce17d988efa411aa00761834601f86
Shanya Hushyar <shanyahushyar12@gmail.com>
2022-01-28 15:51:02 +0300

    list of products UI

---
+++ Assignmet3/lib/src/card.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'data.dart';
+
+class card extends StatefulWidget {
+  const card({Key? key}) : super(key: key);
+
+  @override
+  _cardState createState() => _cardState();
+}
+
+class _cardState extends State<card> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const card1(),
+    );
+  }
+}
+
+class card1 extends StatelessWidget {
+  const card1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: ListView.builder(
+      itemBuilder: (context, index) {
+        return Card(
+          child: Container(
+            height: 80,
+            color: Colors.white,
+            child: Row(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(50),
+                    child: Expanded(
+                      child: Image.asset("assets/1.png"),
+                      flex: 1,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: ListTile(
+                            title: Text("Product Name:"),
+                            subtitle: Text("Product Catigory"),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  flex: 3,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.grey,
+                    child: Center(
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Text(
+                            '200',
+                            textAlign: TextAlign.center,
+                          ))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          elevation: 8,
+          margin: EdgeInsets.all(10),
+        );
+      },
+    ));
+  }
+}
